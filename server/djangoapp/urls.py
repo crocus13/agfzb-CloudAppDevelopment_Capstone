405a8f433e8ffe4f8aa3e32eb7b8a247cb515045
crocus13 <66542067+crocus13@users.noreply.github.com>
2022-03-06 00:12:35 -0800

    updated

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -21,10 +21,14 @@ urlpatterns = [
     path(route='contact', view=views.contact, name='Contact Us'), 
 
     # path for registration
+    path(route='registration', view=views.registration_request, name='Registration'), 
 
     # path for login
+    path(route='login', view=views.login_request, name='Login'), 
 
     # path for logout
+    path(route='logout', view=views.logout_request, name='Logout'), 
+
 
     path(route='', view=views.get_dealerships, name='index')
 
